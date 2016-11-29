65ec63b4ae721414facec7511e5a6df868b8688b
phahs <phil.hahs@gmail.com>
2016-11-28 16:32:54 -0800

    Added unity project

---
+++ Update Color/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Game_Manager : MonoBehaviour
+{
+    public Map mapPrefab;
+    public Canvas mainMenu;
+    public InputField testLevel;
+
+    private bool gameStart;
+    private int level;
+    private Map mapInstance;
+
+    void Start()
+    {
+        level = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameStart)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                RestartGame();
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                EndGame();
+            }
+        }
+    }
+
+    public void BeginGame()
+    {
+        gameStart = true;
+        if(testLevel.text == "")
+        {
+            setLevel(1);
+        }
+        else
+        {
+            setLevel(int.Parse(testLevel.text));
+        }
+        
+        mapInstance = Instantiate(mapPrefab) as Map;
+        mapInstance.GenerateMap(level);
+    }
+
+    public void setLevel(int value)
+    {
+
+        level = value;
+    }
+
+    public void RestartGame()
+    {
+        Destroy(mapInstance.gameObject);
+
+        BeginGame();
+    }
+
+    public void EndGame()
+    {
+        gameStart = false;
+        Destroy(mapInstance.gameObject);
+        mainMenu.gameObject.SetActive(true);
+    }
+
+    public void MenuQuit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Update Color/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour
+{
+    public Coordinates destination;
+    public Vector2 dest;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // screen flash to color of level
+        // do the teleport while screen is the color
+       
+    }
+
+    public Coordinates getDestination()
+    {
+        return destination;
+    }
+
+    public void setDestination(Map_Tile tile)
+    {
+        destination = tile.coord;
+        dest.x = tile.coord.x;
+        dest.y = tile.coord.z;
+    }
+}
